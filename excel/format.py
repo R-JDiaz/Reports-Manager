72df841094659735f4e58344a58ea69fa14f6495
R-JDiaz <diazrj54@gmail.com>
2025-11-26 21:35:10 +0800

    styles

--- excel/format.py
+++ excel/format.py
@@ -1,8 +1,19 @@
 from openpyxl.utils import get_column_letter
+from openpyxl.styles import Font
+
+font = Font(
+    name='Arial',
+    size=11,
+    bold=True
+)
 
 def autofit_selected_columns(ws, column):
     for num in column:
         cellRef = get_column_letter(num)
         cell = ws[f"{cellRef}1"]
-        length = len(str(cell.value)) + 2
+        length = len(str(cell.value)) + 4
         ws.column_dimensions[get_column_letter(num)].width = length
+        setFont(cell)
+
+def setFont(cell, myFont=font):
+    cell.font = myFont
